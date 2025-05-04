fe7437a85883019c065d153b8352b8f083e8c80b
Julien-ser <julien.serbanescu@gmail.com>
2025-05-04 00:23:18 -0400

    stiff

--- backend/mongotest.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-cluster = MongoClient("mongodb+srv://julienserbanescu:<pass>@cluster0.6nnhafa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = cluster["MyDB"]
-collection = db["MyDB"]
-
-collection.insert_one({"name": "Julien", "age": 25, "city": "Paris"})
-print("Data inserted successfully!")
